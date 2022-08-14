a0f6f44adad3480442c2dbb6a702d63c643f554f
talktopete@gmail.com <talktopete@gmail.com>
2022-08-14 14:10:50 -0400

    adding source code to gen art disp

--- src/reallifedata/genart.cljs
+++ src/reallifedata/genart.cljs
@@ -20,29 +20,35 @@
     (reset! projects (js->clj (:items (:body response))))))
 
 ; given a project (string in the form of reallifedata.genartprojects.p1/create
+; and a source (string in the form of the path of the source code
 ; load a modal window containing a canvas and run the project within the canvas asynchronously
-(defn run-project [project]
-  (js/alert project)
+; also display the source below the image
+(defn run-project [project src]
   (let [fun (js/eval (-> project
         (str/replace #"/" ".")
         (str/replace #"-" "_")))]
   (reagent-modals/modal! [
   (fn []
-    (js/alert fun)
     (js/setTimeout #(fun "sketch") 250)
-    [:div {:id "sketch"}])])))
+    ((g/get js/window "loadSrc") src "src")
+    [:div
+     [:div.row {:id "sketch"}]
+     [:div.row
+      [:div.col-md-12
+        [:p "Source"]
+        [:pre#src {:style {:width "100%"}}]]]
+     ])])))
 
 (defn view []
   ((g/get js/window "loadSrc") "genart")
   [:div [:h2 "generative art"]
-   [:div.row]
-   [:div.col-md-10
+   [:div.col-md-12
     [reagent-modals/modal-window]
     [XMasonry {:id "grid" :center true :responsive true}
      (for [project (shuffle @projects)]
         [XBlock {:key (:key project)}
          [:div.tile.panel.panel-default
-          [:a {:on-click #(run-project (:key project))}
+          [:a {:on-click #(run-project (:key project) (:src project))}
             [:div.tileheader.panel-heading [:img {:src (:picture project) :style {:max-width "100%" :width "auto" :height "auto" }}]]
             [:div {:style {:background-color "#0c0" :color "#222" :padding "0px 5px" }} [:h4 (:name project)]]
             [:div.tilebody.panel-body (:description project)]
